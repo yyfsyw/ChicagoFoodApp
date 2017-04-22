04f315a2d87fed596d9cbd37a52cd8139c2cf599
zzyrd <zhangzhihao@mwc-066185.dhcp.missouri.edu>
2017-04-22 17:43:07 -0500

    add NSManager subclass for entity facility and inspection

---
+++ ChicagoFoodApp/ChicagoFoodApp/Facility+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Facility+CoreDataClass.swift
+//  ChicagoFoodApp
+//
+//  Created by zhang  zhihao on 4/22/17.
+//  Copyright © 2017 YUNFEI YANG. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Facility)
+public class Facility: NSManagedObject {
+
+}
---
+++ ChicagoFoodApp/ChicagoFoodApp/Inspection+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Inspection+CoreDataClass.swift
+//  ChicagoFoodApp
+//
+//  Created by zhang  zhihao on 4/22/17.
+//  Copyright © 2017 YUNFEI YANG. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Inspection)
+public class Inspection: NSManagedObject {
+
+}
