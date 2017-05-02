cb853c0af09bc058775eb7433a41f36cf799744c
zzyrd <zhangzhihao@mwc-065000.dhcp.missouri.edu>
2017-05-01 22:52:43 -0500

    Huge updated

--- ChicagoFoodApp/ChicagoFoodApp/JSONParser.swift
+++ ChicagoFoodApp/ChicagoFoodApp/JSONParser.swift
@@ -21,69 +21,77 @@ class JSONParser {
             let dataNodes = root["data"] as? [[Any]]
         {
             for dataNode in dataNodes{
-                if let address = dataNode[14] as? String,
-                   let name = dataNode[9] as? String,
-                   let type = dataNode[12] as? String,
-                   let li = dataNode[11] as? String, let license = Int(li),
-                   let riskvalue = dataNode[13] as? String,
-                   let la = dataNode[22] as? String, let latitude = Double(la),
-                   let lo = dataNode[23] as? String, let longitude = Double(lo)
-                {
-                        let risk: Decimal
+                        //parse facility
+                        let address = dataNode[14] as? String ?? ""
+                        let name = dataNode[9] as? String ?? ""
+                        let type = dataNode[12] as? String ?? ""
+                        let li = dataNode[11] as? String ?? ""
+                        let license = Int(li) ?? -1
+                        let riskvalue = dataNode[13] as? String ?? ""
+                        let la = dataNode[22] as? String ?? ""
+                        let latitude = Double(la) ?? 0.0
+                        let lo = dataNode[23] as? String ?? ""
+                        let longitude = Double(lo) ?? 0.0
                         //manually check risk value
-                        if(riskvalue == "Risk 1 (High)") {
+                        let risk: Decimal
+                        if riskvalue == "Risk 1 (High)" {
                             risk = 1
                         }
-                        else if(riskvalue == "Risk 2 (Medium)") {
+                        else if riskvalue == "Risk 2 (Medium)" {
                             risk = 2
                         }
-                        else if(riskvalue == "Risk 3 (Low)") {
+                        else if riskvalue == "Risk 3 (Low)" {
                             risk = 3
                         }
                         else{
                             risk = 0
                         }
-                        var newFacility = FoodFacility(address: address, name: name, type: type, license: license, risk: risk, latitude: latitude, longitude: longitude)
-
-                            print("address: \(newFacility.address)")
-                            print("the risk value: \(newFacility.risk)")
-                            print("Type of facility: \(newFacility.type)")
-                            print("license ID: \(newFacility.license)")
-                            print("the Name: \(newFacility.name)")
-                            print("latitude: \(newFacility.latitude)")
-                            print("longitude: \(newFacility.longitude)")
                     
-                    //parse inspection
-                    if let inspectID = dataNode[8] as? String, let id = Int(inspectID),
-                       let dateStr = dataNode[18] as? String,
-                       let inspectType = dataNode[19] as? String,
-                       let result = dataNode[20] as? String
-                       {
+                        var newFacility: FoodFacility
+                        //get the facility by name if there is one stored in foodfacilities
+                        let facilityChecking = foodfacilities.first(where: { (element) -> Bool in
+                            return element.name == name
+                            
+                        })
+                        if facilityChecking == nil {
+                            newFacility = FoodFacility(address: address, name: name, type: type, license: license, risk: risk, latitude: latitude, longitude: longitude)
+                        }
+                        else{//force unwrap the facilityChecking because it is not nil
+                            newFacility = facilityChecking!
+                        }
+                        //parse inspection
+                        let inspectID = dataNode[8] as? String ?? ""
+                        let id = Int(inspectID) ?? -1
+                        let dateStr = dataNode[18] as? String ?? ""
+                        let inspectType = dataNode[19] as? String ?? ""
+                        let result = dataNode[20] as? String ?? ""
+                        let violation = dataNode[21] as? String ?? ""
                         //get date, month, and year from dateStr
                         let str_index = dateStr.index(dateStr.startIndex, offsetBy: 10)
                         let realDate = dateStr.substring(to: str_index)
-                        print("the sub is: \(realDate)")
                         dateFormatter.dateFormat = "yyyy/MM/dd"
                         let storedDate = dateFormatter.date(from: realDate)
-                        
-                        //for check violation when it returns nil
-                        let violation = dataNode[21] as? String ?? ""
-                        
                         if let date = storedDate {
-                            newFacility.inspections.append(FoodInspection(type: inspectType, result: result, violation: violation, id: id, date: date))
+                                newFacility.inspections.append(FoodInspection(type: inspectType, result: result, violation: violation, id: id, date: date))
                         }
-                        print("the date is: \(newFacility.inspections[0].date)")
-                        print("Inspect ID: \(newFacility.inspections[0].id)")
-                        print("inspect type: \(newFacility.inspections[0].type)")
-                        print("result: \(newFacility.inspections[0].result)")
-                        print("viloation description: \(newFacility.inspections[0].violation)")
-                    }
-                   foodfacilities.append(newFacility)
-                    
-                }
+                
+                        //when not find any facility in foodfacilities by name, then add new facility to the list
+                        if facilityChecking == nil {
+                            foodfacilities.append(newFacility)
+                        }
+                        else{
+                            
+                            if let index = foodfacilities.index(where: { (element) -> Bool in
+                                return element.name == newFacility.name
+                            }) {
+                                foodfacilities[index] = newFacility
+                            }
+                        }
+                
             }
             
         }
+        print("Task is done")
         return foodfacilities
     }
 }
