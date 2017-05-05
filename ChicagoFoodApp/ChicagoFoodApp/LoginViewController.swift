9ff1bc6702566533b4c7560dfabcab06ba5565fc
zzyrd <zhangzhihao@mwc-067213.dhcp.missouri.edu>
2017-05-05 14:01:41 -0500

    available change the boolean value for favorited in facility entity

--- ChicagoFoodApp/ChicagoFoodApp/LoginViewController.swift
+++ ChicagoFoodApp/ChicagoFoodApp/LoginViewController.swift
@@ -28,11 +28,26 @@ class LoginViewController: UIViewController {
         
         print("-------------------")
         for facility in facilities {
-            print(facility.address ?? "")
-            print(facility.longitude )
-            print(facility.latitude )
+//            print(facility.address ?? "")
+//            print(facility.longitude )
+//            print(facility.latitude )
             print(facility.favorited)
+            facility.favorited = false
+            do {
+                try facility.managedObjectContext?.save()
+            } catch  {
+                let saveError = error as NSError
+                print(saveError)
+            }
+            
+            
         }
+          print("-------------------")
+        for facility in facilities{
+            print(facility.favorited)
+            
+        }
+        
 
         // Do any additional setup after loading the view.
     }
