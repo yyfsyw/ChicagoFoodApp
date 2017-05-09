c0548a26b65fe055846d0e00d0e2e50aa0a8d628
zzyrd <zhangzhihao@mwc-065166.dhcp.missouri.edu>
2017-05-09 16:39:23 -0500

    Getting Stuff Going

--- ChicagoFoodApp/ChicagoFoodApp/AppDelegate.swift
+++ ChicagoFoodApp/ChicagoFoodApp/AppDelegate.swift
@@ -14,17 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        //UIStoryboard.centerViewController()
-        let storyboard = UIStoryboard(name:"Main", bundle:nil)
-        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginViewController
-        self.window?.rootViewController = loginVC
-        
-        return true
-    }
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
