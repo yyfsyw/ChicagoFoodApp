12802ff0021fe8328cd6a1f5afb984e1fbe08ac7
zzyrd <zhangzhihao@mwc-065088.dhcp.missouri.edu>
2017-05-09 22:29:42 -0500

    JUST DONE WITH ALL LIST

---
+++ ChicagoFoodApp/ChicagoFoodApp/AllTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  AllTableViewController.swift
+//  ChicagoFoodApp
+//
+//  Created by zhang  zhihao on 5/9/17.
+//  Copyright © 2017 YUNFEI YANG. All rights reserved.
+//
+
+import UIKit
+
+class AllTableViewController: UITableViewController {
+
+    @IBOutlet var allFacilitiesTable: UITableView!
+    
+    
+     var facilities: [Facility] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Model.sharedInstance.loadData()
+        facilities = Model.sharedInstance.fetchFacilities()
+        print("-------------------")
+        print(facilities.count)
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return facilities.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "allfacilitiescell", for: indexPath)
+        
+        if let cell = cell as? AllFacilityTableViewCell {
+            cell.facilityAddress.text = facilities[indexPath.row].address ?? ""
+            cell.facilityName.text = facilities[indexPath.row].name ?? ""
+            cell.facilityType.text = facilities[indexPath.row].type ?? ""
+
+        }
+
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
